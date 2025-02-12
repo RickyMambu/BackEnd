4db33b039b3c89486ae3372cf12d13a5d3d88561
RickyMambu <rickymambu14@gmail.com>
2025-02-12 09:44:35 +0800

    excercise #3

--- latihan/index.js
+++ latihan/index.js
@@ -1,47 +1,40 @@
-const http = require('http')
+const http = require("http");
+const {getUsers} = require('./users')
+const moment = require('moment')
 const express = require ('express')
 const app = express()
 
-// app.get('/', (req, res) => res.send('Hello World!'))
 
-// //Method
-// app.get('/contoh', (req, res) => {
-//     res.send('request dengan method POST')
-// })
-// app.get('/about',(req,res) => res.status(200).json({
-//     status: 'success',
-//     message: 'About page',
-//     data: []
-// }))
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      status: "success",
+      message: "This is the homepage",
+    });
+  });
+  
+  app.get("/about", (req, res) => {
+    const {exe} = req.query
+    res.status(200).json({
+      status: "success",
+      message: "Response success",
+      description: `Exercise #3`,
+      date: moment().format(),
+    });
+  });
+  
+  app.get("/users", (req, res) => {
+    res.status(200).json({getUsers});
+  });
+  
+  app.get("/:id", (req, res) => {
+    res.status(404).json({
+      status: "not found",
+      message: "Route tidak ditemukan",
+    });
+  });
+  
 
-// app.post('/contoh', (req, res) => {
-//     res.send('request dengan method POST')
-// })
-
-// app.put('/contoh', (req, res) => {
-//     res.send('request dengan method PUT')
-// })
-
-// app.delete('/contoh', (req, res) => {
-//     res.send('request dengan method DELETE')
-// })
-
-// app.patch('/contoh', (req, res) => {
-//     res.send('request dengan method PATCH')
-// })
-
-
-
-//Routing Dinamis
-//1. Menggunakan params
-app.get('/post/:id', (req,res) => res.send(`Artikel ke - ${req.params.id}`))
-//2. Query String - bukan : tapi ?
-app.get('/post', (req,res) => {
-    const {page, sort} = req.query
-    res.send(`Query yang didapatkan adalah: ${page}, sort: ${sort}`)
-})
-
-const hostname = "127.0.0.1"
-const port = 3000
+const hostname = "127.0.0.1";
+const port = 3000;
 app.listen(port, () => {console.log(`Server running at http://${hostname}:${port}/`)})
 
