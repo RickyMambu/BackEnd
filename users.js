eea0f94b303f835b8fb18b1c8190fca0a6d50317
RickyMambu <rickymambu14@gmail.com>
2025-02-19 09:45:04 +0800

    excercise4

---
+++ users.js
@@ -0,0 +1,18 @@
+const users = 
+
+    [
+        {
+          "id": 1,
+          "name": "Bob",       
+        },
+        {
+          "id": 2,
+          "name": "Smith",
+        },
+        {
+          "id": 3,
+          "name": "John",
+          
+        },
+      ]
+module.exports = {users}
